b4a06a5edde0008c6c37865d19ad4cb5b1f1837c
EvanOdell <evanodell91@gmail.com>
2017-08-11 11:39:16 +0100

    update with summary table

--- grepping.R
+++ grepping.R
@@ -11,26 +11,24 @@ for(i in atemp) {
  
 }
 
-### Might not need all this, figure it out soon
-childline.org.uk.txtdf <- as.data.frame(childline.org.uk.txtdf)
-cpchalifax.org.uk.txtdf <- as.data.frame(cpchalifax.org.uk.txtdf)
-carefreekids.org.txtdf <- as.data.frame(carefreekids.org.txtdf)
-healthyyoungmindspennine.nhs.uk.txtdf <- as.data.frame(healthyyoungmindspennine.nhs.uk.txtdf)
-interchangesheffield.org.uk.txtdf <- as.data.frame(interchangesheffield.org.uk.txtdf)
-kidstimefoundation.org.txtdf <- as.data.frame(kidstimefoundation.org.txtdf)
-otrtwickenham.com.txtdf <- as.data.frame(otrtwickenham.com.txtdf)
-ymcadlg.org.txtdf <- as.data.frame(ymcadlg.org.txtdf)
-psspeople.com.txtdf <- as.data.frame(psspeople.com.txtdf)
-middlesbroughandstocktonmind.org.uk.txtdf <- as.data.frame(middlesbroughandstocktonmind.org.uk.txtdf)
-swingsandsmiles.co.uk.txtdf <- as.data.frame(swingsandsmiles.co.uk.txtdf)
-discvol.org.uk.txtdf <- as.data.frame(discvol.org.uk.txtdf)
-ne.org.uk.txtdf <- as.data.frame(ne.org.uk.txtdf)
-southendcarers.co.uk.txtdf <- as.data.frame(southendcarers.co.uk.txtdf)
-ymcadlg.org.txtdf <- as.data.frame(`disc-ymcadlg.org.txtdf`)
-sussexcommunity.org.uk.txtdf <- as.data.frame(sussexcommunity.org.uk.txtdf)
-thewishcentre.org.uk.txtdf <- as.data.frame(thewishcentre.org.uk.txtdf)
+rm(i, atemp)
 
-rm(i, atemp, df_list, `door-ways.co.uk.txtdf`, `disc-vol.org.uk.txtdf`)
+### Might not need all this, figure it out soon
+childline.org.uk.txtdf <- as.data.frame(childline.org.uk.txtdf, stringsAsFactors = FALSE)
+cpchalifax.org.uk.txtdf <- as.data.frame(cpchalifax.org.uk.txtdf, stringsAsFactors = FALSE)
+healthyyoungmindspennine.nhs.uk.txtdf <- as.data.frame(healthyyoungmindspennine.nhs.uk.txtdf, stringsAsFactors = FALSE)
+interchangesheffield.org.uk.txtdf <- as.data.frame(interchangesheffield.org.uk.txtdf, stringsAsFactors = FALSE)
+kidstimefoundation.org.txtdf <- as.data.frame(kidstimefoundation.org.txtdf, stringsAsFactors = FALSE)
+otrtwickenham.com.txtdf <- as.data.frame(otrtwickenham.com.txtdf, stringsAsFactors = FALSE)
+ymcadlg.org.txtdf <- as.data.frame(ymcadlg.org.txtdf, stringsAsFactors = FALSE)
+psspeople.com.txtdf <- as.data.frame(psspeople.com.txtdf, stringsAsFactors = FALSE)
+middlesbroughandstocktonmind.org.uk.txtdf <- as.data.frame(middlesbroughandstocktonmind.org.uk.txtdf, stringsAsFactors = FALSE)
+swingsandsmiles.co.uk.txtdf <- as.data.frame(swingsandsmiles.co.uk.txtdf, stringsAsFactors = FALSE)
+discvol.org.uk.txtdf <- as.data.frame(discvol.org.uk.txtdf, stringsAsFactors = FALSE)
+ne.org.uk.txtdf <- as.data.frame(ne.org.uk.txtdf, stringsAsFactors = FALSE)
+southendcarers.co.uk.txtdf <- as.data.frame(southendcarers.co.uk.txtdf, stringsAsFactors = FALSE)
+sussexcommunity.org.uk.txtdf <- as.data.frame(sussexcommunity.org.uk.txtdf, stringsAsFactors = FALSE)
+thewishcentre.org.uk.txtdf <- as.data.frame(thewishcentre.org.uk.txtdf, stringsAsFactors = FALSE)
 
 df_list <- lapply(ls(), function(x) if (class(get(x)) == "data.frame") get(x))
 
@@ -46,9 +44,24 @@ df$parsed_text <- str_replace_all(df$parsed_text, "[\t]" , " ")
 
 df$parsed_text <- str_replace_all(df$parsed_text, "\\s+" , " ")
 
-df$incredible <- length(grep("Incredible Years", df$parsed_text, ignore.case = TRUE))
+df$mental_health <- lengths(regmatches(df$parsed_text, gregexpr("mental health", df$parsed_text, ignore.case = TRUE)))
 
-df$coping <- length(grep("Coping Cat", df$parsed_text, ignore.case = TRUE))
+df$mental_health
 
-df$triplep <- length(grep("Triple P", df$parsed_text, ignore.case = TRUE))
+df$incredible <- lengths(regmatches(df$parsed_text, gregexpr("incredible years", df$parsed_text, ignore.case = TRUE)))
 
+df$incredible 
+
+df$coping <- lengths(regmatches(df$parsed_text, gregexpr("coping cat", df$parsed_text, ignore.case = TRUE)))
+
+df$coping
+
+df$triplep <- lengths(regmatches(df$parsed_text, gregexpr("triple p", df$parsed_text, ignore.case = TRUE)))
+
+df$triplep
+
+summary_table <- df[c(1,3:6)]
+
+summary_table
+
+write_csv(summary_table, "summary-table.csv")
